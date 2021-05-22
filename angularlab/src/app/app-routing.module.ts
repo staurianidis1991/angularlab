e3c2e755c24caf8c2583413b86740eb76ff8574f
staurianidis1991 <83314398+staurianidis1991@users.noreply.github.com>
2021-05-22 16:18:37 +0300

    Add files via upload

---
+++ angularlab/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { RestComponent } from './rest/rest.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'rest',component:RestComponent},
+  // {path:'',redirectTo:'/rest',pathMatch:'full'},
+  {path:'home',component:HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
